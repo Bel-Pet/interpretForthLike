8c2bc283ee1f56517f9e0ff53f0842b415b52265
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-01-10 18:48:59 +0700

    add write to string

--- interpreter.h
+++ interpreter.h
@@ -3,6 +3,7 @@
 
 #include <map>
 #include "command.h"
+#include <fstream>
 
 class Interpreter {
 public:
@@ -14,16 +15,15 @@ public:
     // Adds a command to the commands map
     bool registerCreator(const std::string& c, creator_t creator);
     // Translates a string into commands and fulfills them
-    void interpret(std::string::iterator& it, std::string::iterator& end);
+    std::string interpret(std::string::iterator& it, std::string::iterator& end);
     // Find and return command else return error
     Command* find_command(std::string::iterator& it, std::string::iterator& end, const std::string& str);
     // Find and return word from string
     std::string find_str(std::string::iterator& it, std::string::iterator& end);
 private:
     Interpreter() = default;
-    std::map<std::string , creator_t> creators_;
+    std::map<std::string, creator_t> creators_;
     std::vector<int> data_;
 };
 
 #endif
-
