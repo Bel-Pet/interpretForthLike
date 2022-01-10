ec3db98d2841e28846873b318f502e8110f4c2b4
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-01-11 02:36:37 +0700

    fix

--- command.cpp
+++ command.cpp
@@ -3,143 +3,61 @@
 
 namespace {
 
-    Command * add_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new Add();
-    }
+    bool add_cmd = Interpreter::getInstance().registerCreator("+", new Add());
 
-    bool add_cmd = Interpreter::getInstance().registerCreator("+", add_creator);
+    bool sub_cmd = Interpreter::getInstance().registerCreator("-", new Sub());
 
-    Command * sub_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new Sub();
-    }
+    bool mod_cmd = Interpreter::getInstance().registerCreator("%", new Mod());
 
-    bool sub_cmd = Interpreter::getInstance().registerCreator("-", sub_creator);
+    bool div_cmd = Interpreter::getInstance().registerCreator("/", new Div());
 
-    Command * mod_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new Mod();
-    }
+    bool mul_cmd = Interpreter::getInstance().registerCreator("*", new Mul());
 
-    bool mod_cmd = Interpreter::getInstance().registerCreator("%", mod_creator);
+    bool more_cmd = Interpreter::getInstance().registerCreator(">", new More());
 
-    Command * div_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new Div();
-    }
+    bool less_cmd = Interpreter::getInstance().registerCreator("<", new Less());
 
-    bool div_cmd = Interpreter::getInstance().registerCreator("/", div_creator);
+    bool equals_cmd = Interpreter::getInstance().registerCreator("=", new Equals());
 
-    Command * mul_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new Mul();
-    }
+    bool dup_cmd = Interpreter::getInstance().registerCreator("dup", new Dup());
 
-    bool mul_cmd = Interpreter::getInstance().registerCreator("*", mul_creator);
+    bool drop_cmd = Interpreter::getInstance().registerCreator("drop", new Drop());
 
-    Command * more_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new More();
-    }
+    bool point_cmd = Interpreter::getInstance().registerCreator(".", new Point());
 
-    bool more_cmd = Interpreter::getInstance().registerCreator(">", more_creator);
+    bool swap_cmd = Interpreter::getInstance().registerCreator("swap", new Swap());
 
-    Command * less_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new Less();
-    }
+    bool rot_cmd = Interpreter::getInstance().registerCreator("rot", new Rot());
 
-    bool less_cmd = Interpreter::getInstance().registerCreator("<", less_creator);
+    bool over_cmd = Interpreter::getInstance().registerCreator("over", new Over());
 
-    Command * equals_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new Equals();
-    }
+    bool emit_cmd = Interpreter::getInstance().registerCreator("emit", new Emit());
 
-    bool equals_cmd = Interpreter::getInstance().registerCreator("=", equals_creator);
+    bool cr_cmd = Interpreter::getInstance().registerCreator(".cr.", new Cr());
 
-    Command * dup_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new Dup();
-    }
+    bool zero_cmd = Interpreter::getInstance().registerCreator("0", new AddNumber());
 
-    bool dup_cmd = Interpreter::getInstance().registerCreator("dup", dup_creator);
+    bool one_cmd = Interpreter::getInstance().registerCreator("1", new AddNumber());
 
-    Command * drop_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new Drop();
-    }
+    bool two_cmd = Interpreter::getInstance().registerCreator("2", new AddNumber());
 
-    bool drop_cmd = Interpreter::getInstance().registerCreator("drop", drop_creator);
+    bool three_cmd = Interpreter::getInstance().registerCreator("3", new AddNumber());
 
-    Command * point_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new Point();
-    }
+    bool four_cmd = Interpreter::getInstance().registerCreator("4", new AddNumber());
 
-    bool point_cmd = Interpreter::getInstance().registerCreator(".", point_creator);
+    bool five_cmd = Interpreter::getInstance().registerCreator("5", new AddNumber());
 
-    Command * swap_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new Swap();
-    }
+    bool six_cmd = Interpreter::getInstance().registerCreator("6", new AddNumber());
 
-    bool swap_cmd = Interpreter::getInstance().registerCreator("swap", swap_creator);
+    bool seven_cmd = Interpreter::getInstance().registerCreator("7", new AddNumber());
 
-    Command * rot_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new Rot();
-    }
+    bool eight_cmd = Interpreter::getInstance().registerCreator("8", new AddNumber());
 
-    bool rot_cmd = Interpreter::getInstance().registerCreator("rot", rot_creator);
+    bool nine_cmd = Interpreter::getInstance().registerCreator("9", new AddNumber());
 
-    Command * over_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new Over();
-    }
+    bool print_string_cmd = Interpreter::getInstance().registerCreator(".\"", new PrintString());
 
-    bool over_cmd = Interpreter::getInstance().registerCreator("over", over_creator);
-
-    Command * emit_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new Emit();
-    }
-
-    bool emit_cmd = Interpreter::getInstance().registerCreator("emit", emit_creator);
-
-    Command * cr_creator(std::string::iterator & it, std::string::iterator & end) {
-        return new Cr();
-    }
-
-    bool cr_cmd = Interpreter::getInstance().registerCreator(".cr.", cr_creator);
-
-    Command * print_string_creator(std::string::iterator & it, std::string::iterator & end) {
-        it++;
-        std::string str;
-        while (it != end) {
-            if (*it == '"')
-                break;
-            str += *it;
-            it++;
-        }
-        if (it == end) {
-            std::stringstream ss;
-            ss << "not at the end of the line closing parenthesis";
-            throw interpreter_error(ss.str());
-        }
-        it++;
-        return new PrintString(str);
-    }
-
-    bool print_string_cmd = Interpreter::getInstance().registerCreator(".\"", print_string_creator);
-
-    Command * add_number_creator(std::string::iterator & it, std::string::iterator & end) {
-        int num = 0;
-        bool false_number = false;
-        while (it != end && *it != ' ') {
-            if (!(*it >= '0' && *it <= '9')) {
-                false_number = true;
-            }
-            num = num * 10 + (int) (*it - '0');
-            it++;
-        }
-        if (false_number) {
-            std::stringstream ss;
-            ss << "not number";
-            throw interpreter_error(ss.str());
-        }
-        return new AddNumber(num);
-    }
-
-    bool add_number_cmd = Interpreter::getInstance().registerCreator("0", add_number_creator);
-
-   Command * if_creator(std::string::iterator & it, std::string::iterator & end) {
+   /*Command * if_creator(std::string::iterator & it, std::string::iterator & end) {
         std::vector<Command *> one_stack_command;
         std::vector<Command *> two_stack_command;
         bool exist_else = false;
@@ -166,15 +84,13 @@ namespace {
             Command* creator = Interpreter::getInstance().find_command(it, end, str);
             one_stack_command.push_back(creator);
         }
-        if (!exist_then) {
-            std::stringstream ss;
-            ss << "no then;";
-            throw interpreter_error(ss.str());
-        }
+        if (!exist_then)
+            throw interpreter_error("no then;");
+
         return new If(one_stack_command, two_stack_command, exist_else);
     }
 
-    bool if_cmd = Interpreter::getInstance().registerCreator("if", if_creator);
+    bool if_cmd = Interpreter::getInstance().registerCreator("if", new If(one_stack_command, two_stack_command, exist_else));
 
     Command * cycle_creator(std::string::iterator & it, std::string::iterator & end) {
         std::vector<Command *> stack_command;
@@ -201,7 +117,7 @@ namespace {
         return new Cycle(stack_command, i);
     }
 
-    bool cycle_cmd = Interpreter::getInstance().registerCreator("do", cycle_creator);
+    bool cycle_cmd = Interpreter::getInstance().registerCreator("do", new Cycle(stack_command, i));*/
 
     /*Command * new_command_creator(std::string::iterator & it, std::string::iterator & end) {
         std::string str;
