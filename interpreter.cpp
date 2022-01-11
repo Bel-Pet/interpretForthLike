6498a2f2986ee5c42197e14b1214d48373c94acb
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-01-11 23:56:11 +0700

    add struct data_

--- interpreter.cpp
+++ interpreter.cpp
@@ -7,30 +7,32 @@ bool Interpreter::registerCreator(const std::string& c, std::shared_ptr<Command>
 }
 
 std::string Interpreter::interpret(std::string::iterator& it, std::string::iterator& end) {
-    std::stringstream result;
-    result << ">";
-    while (it != end) {
-        if (*it == ' '){
-            it++;
+    data_.it = it;
+    data_.end = end;
+    data_.result += '>';
+    while (data_.it != data_.end) {
+        if (*data_.it == ' '){
+            data_.it++;
             continue;
         }
         try {
-            if (isdigit(*it) || (*it == '-' && isdigit(*(it + 1)))) {
-                creators_["0"]->apply(data_, result, it, end);
+            if (isdigit(*data_.it) || (*data_.it == '-' && isdigit(*(data_.it + 1)))) {
+                creators_["0"]->apply(data_);
                 continue;
             }
             std::string str;
-            for (; it < end && !std::isspace(*it); it++) {
-                str += *it;
+            for (; data_.it < data_.end && !std::isspace(*data_.it); data_.it++) {
+                str += *data_.it;
             }
 
-            find_command(str)->apply(data_, result, it, end);
+            find_command(str)->apply(data_);
         } catch (interpreter_error & e) {
-            result << '\n' << e.what();
+            data_.result += "\n";
+            data_.result += e.what();
             break;
         }
     }
-    return result.str();
+    return data_.result;
 }
 
 std::shared_ptr<Command> Interpreter::find_command( const std::string& str) {
