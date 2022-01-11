65ee26fa0f2cdf83cb3df5f45bbdbdba91cdd242
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-01-11 20:51:39 +0700

    add isspace and rework while with for

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,48 +1,42 @@
 #include "interpreter.h"
+#include <utility>
 
-bool Interpreter::registerCreator(const std::string& c, Command* creator) {
-    creators_[c] = creator;
+bool Interpreter::registerCreator(const std::string& c, std::shared_ptr<Command> creator) {
+    creators_[c] = std::move(creator);
     return true;
 }
 
 std::string Interpreter::interpret(std::string::iterator& it, std::string::iterator& end) {
     std::stringstream result;
     result << ">";
-    std::string str;
     while (it != end) {
-        str.clear();
-        // CR: https://en.cppreference.com/w/cpp/string/wide/iswspace
         if (*it == ' '){
             it++;
             continue;
         }
         try {
-            // CR: negative values
-            if (isdigit(*it)) {
-                str = *it;
-                creators_[str]->apply(data_, result, it, end);
+            if (isdigit(*it) || (*it == '-' && isdigit(*(it + 1)))) {
+                creators_["0"]->apply(data_, result, it, end);
                 continue;
             }
-            // CR: ::iswspace
-            // CR: str += c as separate operation
-            it = std::find_if(it, end, [&str](char c){ if (c == ' ') { return true; } str += c; return false;});
-            // CR: use struct Context { data_, result, it, end }
-            find_command(it, end, str, result)->apply(data_, result, it, end);
+            std::string str;
+            for (; it < end && !std::isspace(*it); it++) {
+                str += *it;
+            }
+
+            find_command(str)->apply(data_, result, it, end);
         } catch (interpreter_error & e) {
-            result << '\n';
-            result << e.what();
-            // CR: stop execution on exception
+            result << '\n' << e.what();
+            break;
         }
     }
     return result.str();
 }
 
-Command* Interpreter::find_command(std::string::iterator& it, std::string::iterator& end, const std::string& str, std::stringstream & result) {
+std::shared_ptr<Command> Interpreter::find_command( const std::string& str) {
     auto creators_it = creators_.find(str);
-    if (creators_it == creators_.end()) {
-        std::stringstream ss;
-        ss << " no such command: '" << str << "'";
-        throw interpreter_error(ss.str());
-    }
+    if (creators_it == creators_.end())
+        throw interpreter_error("no such command: '" + str + "'");
+
     return creators_it->second;
 }
