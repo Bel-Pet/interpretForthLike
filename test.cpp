41f46910d2f62202a80f81b46e81cf6c47762559
Bel-Pet <80157339+Bel-Pet@users.noreply.github.com>
2022-01-24 18:29:38 +0700

    delete str

--- test.cpp
+++ test.cpp
@@ -8,365 +8,303 @@ TEST(Numbers_And_Command_Point, Correct_Work_Push_To_Stack) {
 }
 
 TEST(Numbers_And_Command_Point, Not_Command) {
-    std::string str = "d4 4 . .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("d4 4 . .");
     EXPECT_EQ(result, "\nno such command: 'd4'");
 }
 
 TEST(Numbers_And_Command_Point, Incorrect_Work_Add_Number) {
-    std::string str = "5f 4 . .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("5f 4 . .");
     EXPECT_EQ(result, "\nno such command: '5f'");
 }
 
 TEST(Numbers_And_Command_Point, No_Numbers_To_Stack) {
-    std::string str = ".";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret(".");
     EXPECT_EQ(result, "\nError point: not enough numbers");
 }
 
 // Add command
 
 TEST(ArithCommand, Correct_Work_Add) {
-    std::string str = "3 4 + .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("3 4 + .");
     EXPECT_EQ(result, " 7");
 }
 
 TEST(ArithCommand, Incorrect_Work_Add_One_Number) {
-    std::string str = "1 + .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("1 + .");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
-    str = ".";
-    result = Interpreter::getInstance().interpret(str);
+    result = Interpreter::getInstance().interpret(".");
     EXPECT_EQ(result, " 1");
 }
 
 TEST(ArithCommand, Incorrect_Work_Add_No_Numbers) {
-    std::string str = "+ .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("+ .");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
 }
 
 // Sub command
 
 TEST(ArithCommand, Correct_Work_Sub) {
-    std::string str = "3 4 - .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("3 4 - .");
     EXPECT_EQ(result, " 1");
 }
 
 TEST(ArithCommand, Incorrect_Work_Sub_No_Numbers) {
-    std::string str = "- .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("- .");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
 }
 
 TEST(ArithCommand, Incorrect_Work_Sub_One_Number) {
-    std::string str = "1 - .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("1 - .");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
-    str = ".";
-    result = Interpreter::getInstance().interpret(str);
+    result = Interpreter::getInstance().interpret(".");
     EXPECT_EQ(result, " 1");
 }
 
 // Mod command
 
 TEST(ArithCommand, Correct_Work_Mod) {
-    std::string str = "3 4 % .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("3 4 % .");
     EXPECT_EQ(result, " 1");
 }
 
 TEST(ArithCommand, Incorrect_Work_Mod_No_Numbers) {
-    // CR: inline everywhere
-    std::string str = "% .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("% .");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
 }
 
 TEST(ArithCommand, Incorrect_Work_Mod_One_Number) {
-    std::string str = "1 % .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("1 % .");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
-    str = ".";
-    result = Interpreter::getInstance().interpret(str);
+    result = Interpreter::getInstance().interpret(".");
     EXPECT_EQ(result, " 1");
 }
 
 TEST(ArithCommand, Incorrect_Work_Mod_One_Number_Zero) {
-    std::string str = "3 0 % .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("3 0 % .");
     EXPECT_EQ(result, "\nError mod: first number is null");
-    str = ". .";
-    result = Interpreter::getInstance().interpret(str);
+    result = Interpreter::getInstance().interpret(". .");
     EXPECT_EQ(result, " 0 3");
 }
 
 // Div command
 
 TEST(ArithCommand, Correct_Work_Div) {
-    std::string str = "3 4 / .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("3 4 / .");
     EXPECT_EQ(result, " 1");
 }
 
 TEST(ArithCommand, Incorrect_Work_Div_Zero_Number) {
-    std::string str = "/ .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("/ .");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
 }
 
 TEST(ArithCommand, Incorrect_Work_Div_One_Number) {
-    std::string str = "1 / .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("1 / .");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
-    str = ".";
-    result = Interpreter::getInstance().interpret(str);
+    result = Interpreter::getInstance().interpret(".");
     EXPECT_EQ(result, " 1");
 }
 
 TEST(ArithCommand, Incorrect_Work_Div_One_Number_Zero) {
-    std::string str = "3 0 / .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("3 0 / .");
     EXPECT_EQ(result, "\nError div: first number is null");
-    str = ". .";
-    result = Interpreter::getInstance().interpret(str);
+    result = Interpreter::getInstance().interpret(". .");
     EXPECT_EQ(result, " 0 3");
 }
 
 // Mul command
 
 TEST(ArithCommand, Correct_Work_Mul) {
-    std::string str = "3 4 * .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("3 4 * .");
     EXPECT_EQ(result, " 12");
 }
 
 TEST(ArithCommand, Incorrect_Work_Mul_Zero_Number) {
-    std::string str = "* .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("* .");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
 }
 
 TEST(ArithCommand, Incorrect_Work_Mul_One_Number) {
-    std::string str = "1 * .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("1 * .");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
-    str = ".";
-    result = Interpreter::getInstance().interpret(str);
+    result = Interpreter::getInstance().interpret(".");
     EXPECT_EQ(result, " 1");
 }
 
 // Great command
 
 TEST(ArithCommand, Correct_Work_Great) {
-    std::string str = "2 6 > . 6 2 > .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("2 6 > . 6 2 > .");
     EXPECT_EQ(result, " 1 0");
 }
 
 TEST(ArithCommand, Incorrect_Work_Great_Zero_Number) {
-    std::string str = ">";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret(">");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
 }
 
 TEST(ArithCommand, Incorrect_Work_Great_One_Number) {
-    std::string str = "1 > .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("1 > .");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
-    str = ".";
-    result = Interpreter::getInstance().interpret(str);
+    result = Interpreter::getInstance().interpret(".");
     EXPECT_EQ(result, " 1");
 }
 
 // Less command
 
 TEST(ArithCommand, Correct_Work_Less) {
-    std::string str = "2 6 < . 6 2 < .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("2 6 < . 6 2 < .");
     EXPECT_EQ(result, " 0 1");
 }
 
 TEST(ArithCommand, Incorrect_Work_Less_Zero_Number) {
-    std::string str = "<";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("<");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
 }
 
 TEST(ArithCommand, Incorrect_Work_Less_One_Number) {
-    std::string str = "1 < .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("1 < .");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
-    str = ".";
-    result = Interpreter::getInstance().interpret(str);
+    result = Interpreter::getInstance().interpret(".");
     EXPECT_EQ(result, " 1");
 }
 
 // Equal command
 
 TEST(ArithCommand, Correct_Work_Equal) {
-    std::string str = "2 6 = . 7 7 = .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("2 6 = . 7 7 = .");
     EXPECT_EQ(result, " 0 1");
 }
 
 TEST(ArithCommand, Incorrect_Work_Equal_One_Number) {
-    std::string str = "=";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("=");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
 }
 
 TEST(ArithCommand, Incorrect_Work_Equl_One_Number) {
-    std::string str = "1 = .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("1 = .");
     EXPECT_EQ(result, "\nError arithmetic operation: not enough numbers");
-    str = ".";
-    result = Interpreter::getInstance().interpret(str);
+    result = Interpreter::getInstance().interpret(".");
     EXPECT_EQ(result, " 1");
 }
 
 // Dup command
 
 TEST(Other_Command, Correct_Work_Dup) {
-    std::string str = "2 dup . .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("2 dup . .");
     EXPECT_EQ(result, " 2 2");
 }
 
 TEST(Other_Command, Call_Dup_When_Stack_Is_Empty) {
-    std::string str = "dup 2 dup . .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("dup 2 dup . .");
     EXPECT_EQ(result, "\nError optional operator: not enough numbers");
 }
 
 // Drop command
 
 TEST(Other_Command, Correct_Work_Drop) {
-    std::string str = "2 4 drop .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("2 4 drop .");
     EXPECT_EQ(result, " 2");
 }
 
 TEST(Other_Command, Call_Drop_When_Stack_Is_Empty) {
-    std::string str = "drop . .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("drop . .");
     EXPECT_EQ(result, "\nError optional operator: not enough numbers");
 }
 
 // Emit command
 
 TEST(Other_Command, Correct_Work_Emit) {
-    std::string str = "65 emit";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("65 emit");
     EXPECT_EQ(result, " A");
 }
 
 TEST(Other_Command, Incorrect_Work_Emit_No_Number) {
-    std::string str = "emit";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("emit");
     EXPECT_EQ(result, "\nError emit: not enough numbers");
 }
 
 TEST(Other_Command, Incorrect_Work_Emit_Not_Ascii) {
-    std::string str = "-2 emit";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("-2 emit");
     EXPECT_EQ(result, "\nError emit: number on stack is out of ASCII range");
 }
 
 // Swap command
 
 TEST(Other_Command, Correct_Work_Swap) {
-    std::string str = "2 3 swap . .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("2 3 swap . .");
     EXPECT_EQ(result, " 2 3");
 }
 
 TEST(Other_Command, Incorrect_Work_Swap_One_Number) {
-    std::string str = "3 swap . .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("3 swap . .");
     EXPECT_EQ(result, "\nError swap: not enough numbers");
-    str = ".";
-    result = Interpreter::getInstance().interpret(str);
+    result = Interpreter::getInstance().interpret(".");
     EXPECT_EQ(result, " 3");
 }
 
 TEST(Other_Command, Incorrect_Work_Swap_No_Numbers) {
-    std::string str = "swap . .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("swap . .");
     EXPECT_EQ(result, "\nError swap: not enough numbers");
 }
 
 // Rot command
 
 TEST(Other_Command, Correct_Work_Rot) {
-    std::string str = "2 3 4 rot . . .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("2 3 4 rot . . .");
     EXPECT_EQ(result, " 3 2 4");
 }
 
 TEST(Other_Command, Incorrect_Work_Rot_No_Number) {
-    std::string str = "3 4 rot . . .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("3 4 rot . . .");
     EXPECT_EQ(result, "\nError rot: not enough numbers");
-    str = ". .";
-    result = Interpreter::getInstance().interpret(str);
+    result = Interpreter::getInstance().interpret(". .");
     EXPECT_EQ(result, " 4 3");
 }
 
 TEST(Other_Command, Incorrect_Work_Rot_No_Numbers) {
-    std::string str = "rot . . .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("rot . . .");
     EXPECT_EQ(result, "\nError rot: not enough numbers");
 }
 
 // Over command
 
 TEST(Other_Command, Correct_Work_Over) {
-    std::string str = "2 3 over . . .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("2 3 over . . .");
     EXPECT_EQ(result," 2 3 2");
 }
 
 TEST(Other_Command, Incorrect_Work_Over_No_Numbers) {
-    std::string str = "over . . .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("over . . .");
     EXPECT_EQ(result, "\nError over: not enough numbers");
 }
 
 TEST(Other_Command, Incorrect_Work_Over_One_Number) {
-    std::string str = "1 over . . .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret("1 over . . .");
     EXPECT_EQ(result, "\nError over: not enough numbers");
 }
 
 // Cr command
 
 TEST(Other_Command, Correct_Work_Cr) {
-    std::string str = " 23 45 . cr .";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret(" 23 45 . cr .");
     EXPECT_EQ(result, " 45\n 23");
 }
 
 // Print string command
 
 TEST(Other_Command, Correct_Work_PrintString) {
-    std::string str = ".\" wef awd\"";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret(".\" wef awd\"");
     EXPECT_EQ(result, " wef awd");
 }
 
 TEST(Other_Command, Incorrect_Work_PrintString_No_Closing_Bracket) {
-    std::string str = ".\" wef awd";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret(".\" wef awd");
     EXPECT_EQ(result, "\nError .\": no \"");
 }
 
 TEST(Other_Command, Incorrect_Work_PrintString_Two_Closing_Brackets) {
-    std::string str = R"(." wef awd"")";
-    std::string result = Interpreter::getInstance().interpret(str);
+    std::string result = Interpreter::getInstance().interpret(R"(." wef awd"")");
     EXPECT_EQ(result, " wef awd\nno such command: '\"'");
 }
 
